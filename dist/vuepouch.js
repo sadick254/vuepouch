4503334f365f3b0bbc6566e680660ea3cc797215
Sadick <sadickjunior@gmail.com>
2017-07-15 19:19:49 +0300

    build

--- dist/vuepouch.js
+++ dist/vuepouch.js
@@ -1,4 +1,14 @@
-/******/ (function(modules) { // webpackBootstrap
+(function webpackUniversalModuleDefinition(root, factory) {
+	if(typeof exports === 'object' && typeof module === 'object')
+		module.exports = factory();
+	else if(typeof define === 'function' && define.amd)
+		define([], factory);
+	else if(typeof exports === 'object')
+		exports["vuepouch"] = factory();
+	else
+		root["vuepouch"] = factory();
+})(this, function() {
+return /******/ (function(modules) { // webpackBootstrap
 /******/ 	// The module cache
 /******/ 	var installedModules = {};
 /******/
@@ -85,3 +95,4 @@ exports.default = {
 
 /***/ })
 /******/ ]);
+});
